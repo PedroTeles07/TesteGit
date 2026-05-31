3e798da2314e2dc92a78a0b9a9b090c2d7c439e9
Pedro Teles <pedrotelesneto470@gmail.com>
2026-05-31 18:20:41 -0300

    add job's verification

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -4,7 +4,23 @@ string nome = Console.ReadLine();
 Console.WriteLine("Digite sua idade:");
 int idade = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Digite sua profissao:");
-string profissao = Console.ReadLine();
+Console.WriteLine("Você trabalha? (s/n)");
+string resposta = Console.ReadLine().ToLower();
+if(resposta == "s")
+{
+    Console.WriteLine("Digite sua profissao:");
+    string profissao = Console.ReadLine();
 
-Console.WriteLine($"Olá, {nome}! Você tem {idade} anos e trabalha como {profissao}.");
+    Console.WriteLine($"Olá, {nome}! Você tem {idade} anos e trabalha como {profissao}.");
+
+}
+else if(resposta =="n")
+{
+    Console.WriteLine($"Olá, {nome}! Você tem {idade} anos e não trabalha.");
+}
+else
+{
+    Console.WriteLine("Resposta inválida. Por favor, responda com 's' ou 'n'.");
+}
+
+
