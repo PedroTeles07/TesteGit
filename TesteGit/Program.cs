5350a5b56f7323c39450d89653c44df41894747c
Pedro Teles <pedrotelesneto470@gmail.com>
2026-05-31 17:39:44 -0300

    Adicionar arquivos de projeto.

---
+++ TesteGit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite seu nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine($"Olá, {nome}! Você tem {idade} anos.");
